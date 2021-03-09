04c600b6156c5c599144d5c3a1a04c8207239697
bvinson23 <brandonvinson23@gmail.com>
2021-03-09 10:49:44 -0600

    trying to add eventListeners to the journal, not sucessful so far

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,3 +13,12 @@ const startDailyJournal = () => {
 }
 
 startDailyJournal();
+
+const applicationElement = document.querySelector(".dailyjournal");
+
+applicationElement.addEventListener("click", event => {
+    if(event.target.id("#entry--${entry.id}")){
+        console.log("post clicked", event.target.id.split("--"))
+        console.log("the id is", event.target.id.split("--")[1])
+    }
+})
